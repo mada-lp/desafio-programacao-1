e2a16b52d89b4d61b7691bf11b307756db0d27eb
Madalena Pessanha <madalena.lpessanha@gmail.com>
2017-11-08 22:56:08 -0200

    creation of rails app

---
+++ DesafioProgramacaoRails/db/migrate/20171108203605_create_purchases.rb
@@ -0,0 +1,13 @@
+class CreatePurchases < ActiveRecord::Migration[5.0]
+  def change
+    create_table :purchases do |t|
+      t.string :purchaser_name
+      t.string :item_description
+      t.decimal :item_price
+      t.integer :purchase_count
+      t.string :merchant_address
+      t.string :merchant_name
+      t.timestamps
+    end
+  end
+end
